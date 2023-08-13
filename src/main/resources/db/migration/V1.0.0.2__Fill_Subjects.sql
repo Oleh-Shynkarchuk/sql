ec419cd3c696f76afe666ac529658f1554448490
Roman <kovalchuk.roman03@gmail.com>
2023-08-13 18:12:26 +0300

    Added flyway migration

---
+++ src/main/resources/db/migration/V1.0.0.2__Fill_Subjects.sql
@@ -0,0 +1,10 @@
+INSERT INTO public.subject (name,tutor) VALUES ('Compucter science','Pips');
+INSERT INTO public.subject (name,tutor) VALUES ('Relational databases','Mykhailo Golovachov');
+INSERT INTO public.subject (name,tutor) VALUES ('Microservices architecture','Roman Kovalchuk');
+INSERT INTO public.subject (name,tutor) VALUES ('Web frontend development','Ivan Bulavko');
+INSERT INTO public.subject (name,tutor) VALUES ('Web backend development','Roman Kovalchuk');
+INSERT INTO public.subject (name,tutor) VALUES ('Cloud development','Ruslan Gumeniyk');
+INSERT INTO public.subject (name,tutor) VALUES ('Unit testing','Kyrylo Gerasymenko');
+INSERT INTO public.subject (name,tutor) VALUES ('Android development','Andriy Pedosenko');
+INSERT INTO public.subject (name,tutor) VALUES ('No SQL databases','Hlib Kizin');
+INSERT INTO public.subject (name,tutor) VALUES ('UX/UI designing','Oleg Yavorsky');
